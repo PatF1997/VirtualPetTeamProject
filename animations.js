7b24243de328f9f4e15b492bbe7521b610502c4b
Samuel Kynaston <124631801+SamKynaston@users.noreply.github.com>
2023-06-16 10:00:06 +0100

    new animations js, ability to move sites

---
+++ animations.js
@@ -0,0 +1,91 @@
+const pikachu = document.getElementById("pikachu")
+const charmander = document.getElementById("charmander")
+const jigglypuff = document.getElementById("jigglypuff")
+const bulbasaur = document.getElementById("bulbasaur")
+
+let hasClicked = false
+
+//play sounds for pokemon//
+
+const pikachuSound = new Audio('sounds/pikachu.mp3');
+const charmanderSound = new Audio('sounds/charmander.mp3');
+const puffSound = new Audio('sounds/puff.mp3');
+const bulbSound = new Audio('sounds/bulbasaur.mp3');
+
+//Pikachu Events
+pikachu.addEventListener("mousedown", function(){
+    if (hasClicked) {return false}
+    pikachuSound.play()
+    pikachu.classList.add("decrease-size")
+
+    window.location.href = "./pikachu.html";
+
+    hasClicked = true
+})
+pikachu.addEventListener("mouseover", function(){
+    if (hasClicked) {return false}
+
+    pikachu.classList.add("mid-size")
+});
+pikachu.addEventListener("mouseout", function(){
+    if (hasClicked) {return false}
+    
+    pikachu.classList.remove("mid-size")
+});
+
+//Charmander Events
+charmander.addEventListener("mousedown", function(){
+    if (hasClicked) {return false}
+    charmanderSound.play()
+    charmander.classList.add("decrease-size")
+
+    window.location.href = "./charmander.html";
+
+    hasClicked = true
+})
+charmander.addEventListener("mouseover", function(){
+    if (hasClicked) {return false}
+    charmander.classList.add("mid-size")
+});
+charmander.addEventListener("mouseout", function(){
+    if (hasClicked) {return false}
+    charmander.classList.remove("mid-size")
+});
+
+//Jigglypuff Events
+jigglypuff.addEventListener("mousedown", function(){
+    if (hasClicked) {return false}
+    puffSound.play()
+    jigglypuff.classList.add("decrease-size")
+
+    window.location.href = "./jigglypuff.html";
+
+    hasClicked = true
+})
+jigglypuff.addEventListener("mouseover", function(){
+    if (hasClicked) {return false}
+    jigglypuff.classList.add("mid-size")
+});
+jigglypuff.addEventListener("mouseout", function(){
+    if (hasClicked) {return false}
+    jigglypuff.classList.remove("mid-size")
+});
+
+//Bulbasaur Events
+bulbasaur.addEventListener("mousedown", function(){
+    if (hasClicked) {return false}
+    bulbSound.play()
+    bulbasaur.classList.add("decrease-size")
+
+    window.location.href = "./bulbasaur.html";
+
+    hasClicked = true
+})
+bulbasaur.addEventListener("mouseover", function(){
+    if (hasClicked) {return false}
+    bulbasaur.classList.add("mid-size")
+});
+bulbasaur.addEventListener("mouseout", function(){
+    if (hasClicked) {return false}
+    bulbasaur.classList.remove("mid-size")
+});
